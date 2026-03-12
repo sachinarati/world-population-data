494afaab78aa83042cc8174dabdcdfe1260dc155
sachincb001 <bavdhankarsachin@gmail.com>
2026-03-12 10:19:37 +0900

    New Changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use './' so the app works regardless of the GitHub repository name
-  base: './', 
+  base: '/world-population-data/', 
 })
